6446efcac145647cae62037597fbeed856d9be35
Hrant <harvey.mirijanyan@gmail.com>
2017-05-07 15:51:21 -0700

    Remove Dups

--- ch2/removeDups.js
+++ ch2/removeDups.js
@@ -1,15 +1,49 @@
 const Node = require('./node.js');
 
+// class Node {
+//   constructor(val = null) {
+//     this.next = null;
+//     this.val = val;
+//   }
+//
+//   toString(){
+//     console.log({val: this.val, next: this.next});
+//   }
+//
+// }
 
+const removeDups = (node) => {
+  let currentNode = node;
+  while (currentNode.next) {
+    if(currentNode.next.val === currentNode.val){
+      if (!currentNode.next.next) {
+        currentNode.next = null;
+        break;
+      }
+      currentNode.next = currentNode.next.next;
+    }
+    currentNode = currentNode.next;
+  }
+};
 
 let node1 = new Node(11);
 let node2 = new Node(12);
 let node3 = new Node(13);
-let node4 = new Node(15);
-let node5 = new Node(16);
+let node4 = new Node(13);
+let node5 = new Node(17);
 let node6 = new Node(17);
 node1.next = node2;
 node2.next = node3;
 node3.next = node4;
 node4.next = node5;
 node5.next = node6;
+
+// console.log(removeDups(node1));
+removeDups(node1);
+
+let currentNode = node1;
+
+while (currentNode) {
+  currentNode.toString();
+  currentNode = currentNode.next;
+}
