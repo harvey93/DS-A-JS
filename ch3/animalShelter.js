0e62449db39a9d4f9d7e84209d50533d4b609194
Hrant <harvey.mirijanyan@gmail.com>
2017-05-08 09:59:24 -0700

    Add Animal Shelter

---
+++ ch3/animalShelter.js
@@ -0,0 +1,17 @@
+class Node {
+  constructor(val = null) {
+    this.next = null;
+    this.val = val;
+  }
+
+  toString(){
+    console.log({val: this.val, next: this.next});
+  }
+
+}
+
+class AnimalShelter {
+  constructor() {
+
+  }
+}
