107feb866dbb34c96dfb8132ada1c1a71213d9e5
Hrant <harvey.mirijanyan@gmail.com>
2017-05-08 09:06:55 -0700

    Finish plateStack

--- ch3/plateStack.js
+++ ch3/plateStack.js
@@ -6,17 +6,39 @@ class plateStack {
   }
 
   push(val){
-    this.store[this.pointer].push(val);
     if (this.store[this.pointer].length === this.cap) {
       this.store.push([]);
       this.pointer++;
     }
+    this.store[this.pointer].push(val);
+  }
+
+  pop(){
+    if (this.store[this.pointer].length === 0) {
+      this.store.pop();
+      this.pointer--;
+    }
+    this.store[this.pointer].pop();
   }
 }
 
-let stack = new plateStack();
+let stack = new plateStack(5);
 stack.push(5);
 stack.push(6);
 stack.push(7);
 stack.push(8);
+stack.push(10);
+
+stack.push(3);
+stack.push(4);
+stack.push(5);
+
+stack.pop();
+stack.pop();
+stack.pop();
+stack.pop();
+stack.pop();
+
+// stack.push(4);
+
 console.log(stack.store);
