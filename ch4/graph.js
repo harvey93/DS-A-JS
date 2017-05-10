3f2fe9566f7cb0aa480489f6e8be9eaeee4d93e8
Hrant <harvey.mirijanyan@gmail.com>
2017-05-10 12:46:37 -0700

    Add maxProfit

--- ch4/graph.js
+++ ch4/graph.js
@@ -4,12 +4,31 @@ class Vertex {
     this.inEdges = [];
     this.outEdges = [];
   }
-
-
 }
 
 class Edge {
-  constructor() {
+  constructor(fromVert, toVert, cost = 1) {
+    this.fromVert = fromVert;
+    this.toVert = toVert;
+    this.cost = cost;
 
+    toVert.inEdges.push(this);
+    fromVert.outEdges.push(this);
   }
+
+  destroy(){
+    let inIndex = this.toVert.inEdges.indexOf(this);
+    let outIndex = this.fromVert.outEdges.indexOf(this);
+    this.toVert.inEdges.splice(inIndex);
+    this.fromVert.outEdges.splice(outIndex);
+    this.toVert = null;
+    this.fromVert = null;
+  }
+
+
 }
+
+// self.to_vertex.in_edges.delete(self)
+// self.to_vertex = nil
+// self.from_vertex.out_edges.delete(self)
+// self.from_vertex = nil
--- ch4/test.js
+++ ch4/test.js
@@ -26,8 +26,74 @@ function Word(name) {
   };
 
 }
-let word = new Word('john');
 
-console.log(word.getName());
+var maxProfit = function(prices) {
+    let max = 0;
+    let min = prices[0];
+    let maxProfit = 0;
+    for(let i = 1; i < prices.length; i++){
+        let currentPrice = prices[i];
+        let currentProfit = currentPrice - min;
+        if(currentPrice < min){
+            min = currentPrice;
+        }
+        if(currentProfit > maxProfit){
+            maxProfit = currentProfit;
+        }
+    }
+
+    return maxProfit;
+};
+
+// function calcWrap(numWrap, wrapCount){
+//     let extra = 0;
+//     let count = wrapCount;
+//     while (count / numWrap > 1){
+//        extra += Math.floor(count / numWrap);
+//        count = count % numWrap;
+//        count += extra;
+//     }
+//     return extra;
+// }
+
+
+// function calcWrap(minWrap, tickets){
+//   let ticketCount = tickets;
+//   let extra = 0;
+//   while (ticketCount > 0) {
+//     if (ticketCount / minWrap < 1) {
+//       break;
+//     }
+//     let newCakes = Math.floor((tickets / minWrap));
+//     extra += newCakes;
+//     ticketCount = ticketCount % minWrap;
+//   }
+//   return [extra, ticketCount];
+// }
+//
+// console.log(calcWrap(5, 5));
+
+// let word = new Word('john');
+
+// console.log(word.getName());
 
 // parens(3);
+
+// function StairCase(n) {
+//     let empty = n - 1;
+//     let hashTag = 1;
+//     let res = [];
+//     while(hashTag <= n){
+//         let current = [];
+//         for(let i = 0; i < empty; i++){
+//             current.push(" ");
+//         }
+//         for(let j = 0; j < hashTag; j++){
+//             current.push("#");
+//         }
+//         res.push(current.join(''));
+//         empty--;
+//         hashTag++;
+//     }
+//     console.log(res.join("\n"));
+// }
