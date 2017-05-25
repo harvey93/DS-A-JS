19fbac8dbe3cd9581c20c7ff8bb923474b93ab4c
Hrant <harvey.mirijanyan@gmail.com>
2017-05-24 20:57:54 -0700

    Add checkBst

--- ch4/bst.js
+++ ch4/bst.js
@@ -5,3 +5,48 @@ class Node {
     this.right = null;
   }
 }
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+}
+
+let node1 = new Node("5");
+let node2 = new Node("3");
+let node3 = new Node("8");
+let node4 = new Node("2");
+let node5 = new Node("4");
+let node6 = new Node("6");
+let node7 = new Node("9");
+let node8 = new Node("1");
+
+node1.left = node2;
+// node1.right = node3;
+//
+// node2.left = node4;
+// node2.right = node5;
+//
+// node3.left = node6;
+// node3.right = node7;
+//
+// node4.left = node8;
+
+
+const checkBST = (node, min = null, max = null) => {
+  if (node === null) {
+    return true;
+  }
+  if ((min !== null && node.data <= min) ||
+    (max !== null && node.data > min)){
+      return false;
+  }
+  if (!checkBST(node.left, min, node.data) ||
+      !checkBST(node.right, node.data, max)) {
+      return false;
+  }
+
+  return true;
+};
+
+console.log(checkBST(node1));
--- ch4/tree.js
+++ ch4/tree.js
@@ -1,8 +1,19 @@
 class Node {
+
   constructor(data) {
     this.data = data;
     this.left = null;
     this.right = null;
+    this.arr = [];
+  }
+
+  static height(node){
+    if (!node) {
+      return 0;
+    }
+    let leftHeight = this.height(node.left);
+    let rightHeight = this.height(node.right);
+    return Math.max(leftHeight, rightHeight) + 1;
   }
 
   inOrderPrint(){
@@ -34,25 +45,55 @@ class Node {
     }
     console.log(this.data);
   }
+
 }
 
-let node1 = new Node("1");
-let node2 = new Node("2");
-let node3 = new Node("3");
-let node4 = new Node("4");
-let node5 = new Node("5");
+let node1 = new Node("5");
+let node2 = new Node("3");
+let node3 = new Node("8");
+let node4 = new Node("2");
+let node5 = new Node("4");
 let node6 = new Node("6");
-let node7 = new Node("7");
+let node7 = new Node("9");
+let node8 = new Node("1");
 
 node1.left = node2;
-node1.right = node3;
+// node1.right = node3;
+//
+// node2.left = node4;
+// node2.right = node5;
+//
+// node3.left = node6;
+// node3.right = node7;
+//
+// node4.left = node8;
 
-node2.left = node4;
-node2.right = node5;
 
-node3.left = node6;
-node3.right = node7;
 
+
+//    5
+//   3   8
+// 2  4 6  9
+// 1
+
+// let arr = [];
+
+
+// const inOrderArray = (node, arr = []) => {
+//   if(node.left){
+//     inOrderArray(node.left, arr);
+//   }
+//   arr.push(node.data);
+//
+//   if(node.right){
+//     inOrderArray(node.right, arr);
+//   }
+//   return arr;
+// };
+
+
+// console.log(inOrderArray(node1));
 // node1.inOrderPrint();
 // node1.preOrderPrint();
-node1.postOrderPrint();
+// node1.postOrderPrint();
+// console.log(Node.height(node3));
