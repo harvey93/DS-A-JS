f7df3c59b58482159ea68a95acbb48381d3c37a3
Hrant <harvey.mirijanyan@gmail.com>
2017-05-24 21:28:03 -0700

    Finish checkBST

--- ch4/bst.js
+++ ch4/bst.js
@@ -1,6 +1,6 @@
 class Node {
   constructor(val) {
-    this.value = val;
+    this.data = val;
     this.left = null;
     this.right = null;
   }
@@ -12,25 +12,25 @@ class BST {
   }
 }
 
-let node1 = new Node("5");
-let node2 = new Node("3");
-let node3 = new Node("8");
-let node4 = new Node("2");
-let node5 = new Node("4");
-let node6 = new Node("6");
-let node7 = new Node("9");
-let node8 = new Node("1");
+let node1 = new Node(5);
+let node2 = new Node(3);
+let node3 = new Node(8);
+let node4 = new Node(2);
+let node5 = new Node(4);
+let node6 = new Node(6);
+let node7 = new Node(9);
+let node8 = new Node(1);
 
 node1.left = node2;
-// node1.right = node3;
-//
-// node2.left = node4;
-// node2.right = node5;
-//
-// node3.left = node6;
-// node3.right = node7;
-//
-// node4.left = node8;
+node1.right = node3;
+
+node2.left = node4;
+node2.right = node5;
+
+node3.left = node6;
+node3.right = node7;
+
+node4.left = node8;
 
 
 const checkBST = (node, min = null, max = null) => {
@@ -38,7 +38,7 @@ const checkBST = (node, min = null, max = null) => {
     return true;
   }
   if ((min !== null && node.data <= min) ||
-    (max !== null && node.data > min)){
+    (max !== null && node.data > max)){
       return false;
   }
   if (!checkBST(node.left, min, node.data) ||
@@ -49,4 +49,13 @@ const checkBST = (node, min = null, max = null) => {
   return true;
 };
 
+// const test = (num, min = null, max = null) => {
+//   if(num < 1){
+//     return num;
+//   }
+//   console.log(min);
+//   test(num - 1, num, 10);
+// };
+
+// console.log(test(4));
 console.log(checkBST(node1));
