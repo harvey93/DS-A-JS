51f632c684516ddaea3d71352f811d1f11a6ca01
Hrant <harvey.mirijanyan@gmail.com>
2017-05-27 14:23:34 -0700

    Add getMin/Max

--- ch4/bst.js
+++ ch4/bst.js
@@ -5,6 +5,41 @@ class Node {
     this.right = null;
   }
 
+  static checkBST(node, min = null, max = null) {
+    if(!node) {
+      return true;
+    }
+    if((min && node.data <= min) 
+      || max && node.data > max) {
+        return false;
+      }
+
+    if(!Node.checkBST(node.left, min, node.data) ||
+      !Node.checkBST(node.right, node.data, max)){
+        return false;
+      }
+
+    return true;
+  }
+
+  deleteBST(val, parent = null){
+    if(this.data === val){
+      console.log(parent);
+      console.log(this);
+      return this;
+    }
+    if(val > this.data){
+      if(this.right){
+        return this.right.deleteBST(val, this);
+      }
+    }else {
+      if(this.left){
+        return this.left.deleteBST(val , this);
+      }
+    }
+    return -1;
+  }
+
   addBST(node){
     if(node.data > this.data){
       if(this.right === null){
@@ -33,6 +68,23 @@ class Node {
 
     return arr;
   }
+
+  getMin(){
+    if(this.left === null){
+      return this.data;
+    }else {
+      return this.left.getMin();
+    }
+  }
+
+  getMax(){
+    if(this.right === null){
+      return this.data;
+    } else {
+      return this.right.getMax();
+    }
+  }
+
 }
 
 
@@ -41,9 +93,11 @@ let node2 = new Node(8);
 let node3 = new Node(30);
 let node4 = new Node(3);
 let node5 = new Node(12);
-let node6 = new Node(15);
+let node6 = new Node(16);
 let node7 = new Node(2);
 let node8 = new Node(0);
+let node9 = new Node(55);
+let node10 = new Node(80);
 
 node1.addBST(node2);
 node1.addBST(node5);
@@ -52,52 +106,14 @@ node1.addBST(node3);
 node1.addBST(node8);
 node1.addBST(node6);
 node1.addBST(node7);
-console.log(node1.inOrderTraversal());
+node1.addBST(node9);
+node1.addBST(node10);
+// console.log(node1.inOrderTraversal());
+// console.log(node1.getMax());
 
-// let node1 = new Node(5);
-// let node2 = new Node(3);
-// let node3 = new Node(8);
-// let node4 = new Node(2);
-// let node5 = new Node(4);
-// let node6 = new Node(6);
-// let node7 = new Node(9);
-// let node8 = new Node(1);
-
-// node1.left = node2;
-// node1.right = node3;
-
-// node2.left = node4;
-// node2.right = node5;
-
-// node3.left = node6;
-// node3.right = node7;
-
-// node4.left = node8;
+node1.deleteBST(2);
 
 
-const checkBST = (node, min = null, max = null) => {
-  if (node === null) {
-    return true;
-  }
-  if ((min !== null && node.data <= min) ||
-    (max !== null && node.data > max)){
-      return false;
-  }
-  if (!checkBST(node.left, min, node.data) ||
-      !checkBST(node.right, node.data, max)) {
-      return false;
-  }
+// console.log(Node.checkBST(node1));
 
-  return true;
-};
-
-// const test = (num, min = null, max = null) => {
-//   if(num < 1){
-//     return num;
-//   }
-//   console.log(min);
-//   test(num - 1, num, 10);
-// };
-
-// console.log(test(4));
 // console.log(checkBST(node1));
