74002f7a5434bc4282a8ea29b10e8fa2205a188e
Hrant <harvey.mirijanyan@gmail.com>
2017-05-31 20:28:17 -0700

    Add heap

--- Sorting/inPlaceQSort.js
+++ Sorting/inPlaceQSort.js
@@ -4,6 +4,7 @@ const quickSort = (arr, lo, hi) => {
     // return j;
     quickSort(arr, lo, j - 1);
     quickSort(arr, j + 1, hi);
+    return arr;
 }; 
 
 const partition = (arr, lo, hi) => {
@@ -37,10 +38,10 @@ const exch = (arr, i, j ) => {
     return arr;
 };
 
-let array = [6, 15 ,1, 2, -5, 9, 0, 12, 4, 121];
+let array = [6, 15 ,1, 2,2, 1, -5, 9, 0, 12, 4, 121];
 
-quickSort(array, 0, array.length - 1);
-console.log(array);
+
+console.log(quickSort(array, 0, array.length - 1));
 // console.log(exch([1,2,3,4], 0, 3));
 
 
