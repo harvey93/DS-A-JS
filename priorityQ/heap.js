303e47b308efee2a142eb37d47a69b9f3f62bc9b
Hrant <harvey.mirijanyan@gmail.com>
2017-06-01 12:46:49 -0700

    Add less; exch; and swim opps

--- priorityQ/heap.js
+++ priorityQ/heap.js
@@ -1,5 +1,29 @@
-class BinaryMaxHeap {
+class BMAxHeap {
     constructor(){
-        
+        this.store = [];
+    }
+
+    swim(k){
+        while (k > 1 && this.less(k / 2, k)){
+            exch(k, k /2);
+            k = Math.floor(k / 2);
+        }
+    }
+
+    less(i, j){
+        return this.store[i] < this.store[j];
+    }
+
+    exch(i, j){
+        let temp = this.store[i];
+        this.store[i] = this.store[j];
+        this.store[j] = temp;
     }
 }
+
+const exch = (arr, i, j ) => {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+    return arr;
+};
