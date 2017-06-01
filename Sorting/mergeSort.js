437b3ad1311ebce749e86b75a0be6b01b0330139
Hrant <harvey.mirijanyan@gmail.com>
2017-05-31 17:38:43 -0700

    Add sorting

---
+++ Sorting/mergeSort.js
@@ -0,0 +1,8 @@
+
+
+const merge = (arr1, arr2) => {
+    
+};
+
+let arr1 = [1,4,6,7];
+let arr2 = [2,3,8,9];
--- ch4/avlTree.js
+++ ch4/avlTree.js
@@ -14,6 +14,16 @@ class AVLTree {
         this.root = AVLTree.insert(this.root, value);
     }
 
+    static singleRotation(root, dir){
+        let otherDir = dir === 0 ? 1 : 0;
+        let newRoot = root.subtrees[otherDir];
+
+        root.subtrees[otherDir] = newRoot.subtrees[dir];
+        newRoot.subtrees[dir] = root;
+
+        return newRoot;
+    }
+
     static insert(node, value){
         if(node) {
             let dir = value < node.value ? 0 : 1;
@@ -24,13 +34,39 @@ class AVLTree {
             return new AVLTreeNode(value);
         }
     }
+
+    printArr(node = this.root, arr = []) {
+        arr.push(node.value);
+        if(node.subtrees[0]) {
+            this.printArr(node.subtrees[0], arr);
+        }else {
+            arr.push("X");
+        }
+
+        if(node.subtrees[1]) {
+            this.printArr(node.subtrees[1], arr);
+        } else {
+            arr.push("X");
+        }
+
+        return arr;
+    }
+    
 }
 
 let myTree = new AVLTree();
 
 myTree.insert(3);
-myTree.insert(4);
 myTree.insert(5);
 myTree.insert(6);
+myTree.insert(4);
 
-console.log(myTree);
+// console.log(myTree.root);
+
+// AVLTree.singleRotation(myTree.root, 0);
+
+console.log(myTree.printArr());
+
+    // 3                           5
+    //     5           =>      3       6
+    // 4       6                     4
