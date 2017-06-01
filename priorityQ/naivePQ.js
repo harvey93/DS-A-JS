3c1dcee83b1b6412b296bbd679c9ce120c10bc2c
Hrant <harvey.mirijanyan@gmail.com>
2017-05-31 20:50:31 -0700

    Finish elementary PQ

--- priorityQ/naivePQ.js
+++ priorityQ/naivePQ.js
@@ -1,3 +1,42 @@
-class maxPQ {
-    
+class unMinPQ {
+    constructor(){
+        this.store = [];
+    }
+
+    insert(val) {
+        this.store.push(val);
+    }
+
+    remMax(){
+        let maxI = this.getMaxIdx();
+        this.store.splice(maxI, 1);
+    }
+
+    getMaxIdx(){
+        let currentMax = this.store[0];
+        let currentMaxI = 0;
+        this.store.forEach((el, idx) => {
+            if(el > currentMax){
+                currentMax = el;
+                currentMaxI = idx;
+            }
+        });
+        return currentMaxI;
+    }
 }
+
+let myPQ = new unMinPQ();
+
+myPQ.insert(5);
+myPQ.insert(1);
+myPQ.insert(15);
+myPQ.insert(2);
+myPQ.insert(0);
+
+myPQ.remMax();
+myPQ.remMax();
+myPQ.remMax();
+
+console.log(myPQ);
+
+
