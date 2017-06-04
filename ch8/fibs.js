dc679702edbc51fcc76408b55050e25ca441c355
Hrant <harvey.mirijanyan@gmail.com>
2017-06-04 16:31:22 -0700

    Add memoization for fibs

--- ch8/fibs.js
+++ ch8/fibs.js
@@ -8,9 +8,24 @@ const fibs = (n) => {
     return fibs(n - 1) + fibs(n - 2);
 };
 
-const fibsDP = (n) => {
-    
+const fibsMemo = (n, map = new Map()) => {
+    if(n === 0) {
+        return 0;
+    }
+    if (n === 1) {
+        return 1;
+    }
+    if(map.has(n)){
+        return map.get(n);
+    }
+    map.set(n, fibsMemo(n - 1, map) + fibsMemo(n - 2, map));
+    return fibsMemo(n - 1, map) + fibsMemo(n - 2, map);
+};
+
+const fibsTab = (n) => {
+
 };
 
 console.log(fibs(9));
-console.log(fibsDP(9));
+console.log(fibsMemo(9));
+console.log(fibsTab(9));
