52d15c76d794671c76d6fef3532b6f6a7cfdd2d1
Hrant <harvey.mirijanyan@gmail.com>
2017-06-04 00:27:00 -0700

    Add prodNoDiv

--- Problems/productify.js
+++ Problems/productify.js
@@ -7,8 +7,23 @@ const productify = arr => {
 };
 
 const prodNoDiv = arr => {
+    let products = [];
+    for(let i = 0; i < arr.length; i++) {
+        products.push(1);
+    }
+    let lowerProd = 1;
+    for(let i = 0; i < arr.length; i++) {
+        products[i] *= lowerProd;
+        lowerProd *= arr[i];
+    }
 
-    return arr;
+    let upperProduct = 1;
+    for(let i = arr.length - 1; i >= 0; i--) {
+        products[i] *= upperProduct;
+        upperProduct *= arr[i];
+
+    }
+    return products;
 };
 
 console.log(productify([1,2,3,4]));
