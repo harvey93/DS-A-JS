d08e92c91bfb993897a48465d5ff2feda90a50f5
Hrant <harvey.mirijanyan@gmail.com>
2017-06-06 21:02:25 -0700

    Add memoization to canWin

--- ch8/canWin.js
+++ ch8/canWin.js
@@ -1,5 +1,15 @@
-const canWin = (arr, num) => {
-    
+const canWin = (arr, i, seen = new Set()) => {
+    if(arr[i] === 0) {
+        return true;
+    }
+    seen.add(i);
+    if(i + arr[i] < arr.length && !seen.has(i + arr[i])) {
+        return canWin(arr, i + arr[i], seen);
+    }
+    if(i - arr[i] > 0 && !seen.has(i - arr[i])) {
+        return canWin(arr, i - arr[i], seen);
+    }
+    return false;
 };
 
-console.log(canWin([1,0,1], 0));
+console.log(canWin([1,1,1], 0));
