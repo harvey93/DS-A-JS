c3638cb72b8764a2bbc96e8be6e68f95f0255ce5
Hrant <harvey.mirijanyan@gmail.com>
2017-06-07 16:29:57 -0700

    Add break statement to moveZeroes

--- Problems/moveZeroes.js
+++ Problems/moveZeroes.js
@@ -2,13 +2,17 @@ const moveZeroes = arr => {
     let i = 0;
     let j = arr.length - 1;
     while (i < j) {
-        while(i > 0) {
+        while(arr[i] > 0) {
             i++;
         }
-        while(j > 0) {
+        while(arr[j] === 0) {
             j--;
         }
+        if(i > j) {
+            break;
+        }
         exch(arr, i, j);
+        // console.log(arr);
     }
     return arr;
 };
@@ -19,4 +23,7 @@ const exch = (arr, i, j) => {
     arr[j] = temp;
 };
 
+// let arr = [1, 2, 0, 3, 4, 0, 5, 6, 0];
+// exch(arr,2,7);
+// console.log(arr);
 console.log(moveZeroes([1, 2, 0, 3, 4, 0, 5, 6, 0]));
