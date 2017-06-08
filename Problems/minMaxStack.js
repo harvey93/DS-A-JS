a638d54a234b7457d3d38ba81f6df1badff81e21
Hrant <harvey.mirijanyan@gmail.com>
2017-06-08 15:24:54 -0700

    Add getMin

--- Problems/minMaxStack.js
+++ Problems/minMaxStack.js
@@ -16,19 +16,30 @@ class MinMaxStack {
     }
 
     remove(){
-        this.store.pop();
-        this.max = this.peek().max;
-        this.min = this.peek().min;
+        let res = this.store.pop();
+        if(this.isEmpty()){
+            this.max = null;
+            this.min = null;
+        } else {
+            this.max = this.peek().max;
+            this.min = this.peek().min;
+        }
+        return res;
     }
 
     peek(){
         return this.store[this.store.length - 1];
     }
+
+    isEmpty() {
+        return this.store.length === 0;
+    }
 }
 
 // let stack = new MinMaxStack();
+// stack.insert(3);
 // stack.insert(1);
-// stack.insert(4);
+// stack.insert(6);
 // stack.remove();
 // stack.insert(2);
 // stack.insert(10);
--- Problems/minMaxStackQueue.js
+++ Problems/minMaxStackQueue.js
@@ -11,7 +11,39 @@ class MinMaxStackQueue {
     }
 
     remove(){
-        
+        if(this.popStack.isEmpty()){
+            while(!this.pushStack.isEmpty()) {
+                this.popStack.insert(this.pushStack.remove().val);
+            }
+            return this.popStack.remove();
+        }
+    }
+
+    getMin(){
+        let min1 = null;
+        let min2 = null;
+        if(!this.pushStack.isEmpty()){
+            min1 = this.pushStack.min;
+        }
+        if(!this.popStack.isEmpty()){
+            min2 = this.popStack.min;
+        }
+        // console.log(min1);
+        // console.log(min2);
+
+        if(min1 !== null && min2 !== null) {
+            // console.log('both');
+            return min1 < min2 ? min1 : min2;
+        } else if (min1 !== null) {
+            // console.log('1');
+            return min1;
+        } else if (min2 !== null) {
+            // console.log('2');
+            return min2;
+        } else {
+            // console.log('none');
+            return null;
+        }
     }
 }
 
@@ -21,4 +53,9 @@ queue.insert(1);
 queue.insert(4);
 queue.insert(0);
 queue.insert(6);
-console.log(queue);
+// queue.remove();
+queue.insert(10);
+// queue.remove();
+
+console.log(queue.getMin());
+// queue.getMin();
