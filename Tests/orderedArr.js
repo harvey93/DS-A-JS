7e97a8c7db3999456ea08cccc32c2f583bd9bf3f
Hrant <harvey.mirijanyan@gmail.com>
2017-06-13 13:59:01 -0700

    Remove inPLaceOA2

--- Tests/orderedArr.js
+++ Tests/orderedArr.js
@@ -15,6 +15,17 @@ const inPlaceOA = (table, arr) => {
     return arr;
 };
 
+// const inPlaceOA2 = (table, arr) => {
+//     for(let i = 0; i < arr.length; i++) {
+//         let tableI = table[i];
+//         if (tableI < i) {
+//             tableI = table[tableI];
+//         }
+//         exch(tableI, i, arr);
+//     }
+//     return arr;
+// };
+
 const exch =(i, j, arr) => {
     let temp = arr[i];
     arr[i] = arr[j];
@@ -22,12 +33,12 @@ const exch =(i, j, arr) => {
 };
 
 // let arr = ["a", "b", "c", "d", "e"];
-let order = [0,9,2,8,7,3,6,4,1,5];
+let order = [4,9,1,8,7,3,6,0,2,5];
 
 // Tests
 
 
 console.log(inPlaceOA(order, ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]));
 console.log(orderedArr(order, ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]));
-
+// [ 'e', 'j', 'b', 'i', 'h', 'd', 'g', 'a', 'c', 'f' ]
 
