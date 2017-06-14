fc4dba77e005a9a0682babac91842191ac71535b
Hrant <harvey.mirijanyan@gmail.com>
2017-06-14 16:31:02 -0700

    Add hashDisctionary

--- Problems/hashDictionary.js
+++ Problems/hashDictionary.js
@@ -1,5 +1,17 @@
 const hashDictionary = hash => {
-    
+    let files = [];
+    for(let key in hash) {
+        if( typeof hash[key] === 'object'){
+           let folder = key;
+           let nestedFiles = hashDictionary(hash[key]);
+           nestedFiles.forEach(file => {
+            files.push(`${folder}/${file}`);
+           });
+        } else {
+            files.push(key);
+        }
+    }
+    return files;
 };
 
 let files = {
@@ -17,3 +29,4 @@ let files = {
 };
 
 console.log(hashDictionary(files));
+// hashDictionary(files);
