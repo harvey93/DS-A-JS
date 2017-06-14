499bf5d6a75b97467b76896366400fdb4897a61d
Hrant <harvey.mirijanyan@gmail.com>
2017-06-14 16:08:36 -0700

    Add files

---
+++ Problems/hashDictionary.js
@@ -0,0 +1,19 @@
+const hashDictionary = hash => {
+    
+};
+
+let files = {
+  'a' : {
+    'b' : {
+      'c' : {
+        'd' : {
+          'e' : true
+        },
+
+        'f' : true
+      }
+    }
+  }
+};
+
+console.log(hashDictionary(files));
