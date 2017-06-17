ae5150e1605a45997517728d11b1a24c09603bca
Hrant <harvey.mirijanyan@gmail.com>
2017-06-16 20:39:02 -0700

    Start recRevStr

--- Problems/recReverse.js
+++ Problems/recReverse.js
@@ -8,4 +8,14 @@ const recRev = (arr, i = 0, j = arr.length - 1) => {
     return recRev(arr, i + 1, j - 1);
 };
 
-console.log(recRev("hello".split('')));
+const recRevStr = (str, i = 0, j = str.length - 1) => {
+    if (i === j) {
+        return str;
+    }
+    let temp = str[0];
+    return temp;
+    // return recRevStr(str, i + 1, j - 1);
+};
+
+// console.log(recRev("hello".split('')));
+console.log(recRevStr("hello"));
