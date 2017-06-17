7dfb4040b52d93c6feb11afce0a34bf6cfb84ee3
Hrant <harvey.mirijanyan@gmail.com>
2017-06-16 20:48:25 -0700

    Add recReverseStr

--- Problems/recReverse.js
+++ Problems/recReverse.js
@@ -12,10 +12,14 @@ const recRevStr = (str, i = 0, j = str.length - 1) => {
     if (i === j) {
         return str;
     }
-    let temp = str[0];
-    return temp;
-    // return recRevStr(str, i + 1, j - 1);
+    let first = str[i];
+    let last = str[j];
+    // str = last + str.slice(i + 1, j) + first;
+    str = str.substring(0,i) + last + str.substring(i + 1, j) + first + str.substring(j + 1, str.length);
+    // return str;
+    return recRevStr(str, i + 1, j - 1);
 };
 
 // console.log(recRev("hello".split('')));
 console.log(recRevStr("hello"));
+console.log(recRevStr("abcdefghijklm"));
