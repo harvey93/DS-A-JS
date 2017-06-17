3668ca9435b50e47fb47d2ed199ef93b184fd3e5
Hrant <harvey.mirijanyan@gmail.com>
2017-06-16 20:48:42 -0700

    Remove comments from recReverseString

--- Problems/recReverse.js
+++ Problems/recReverse.js
@@ -14,12 +14,9 @@ const recRevStr = (str, i = 0, j = str.length - 1) => {
     }
     let first = str[i];
     let last = str[j];
-    // str = last + str.slice(i + 1, j) + first;
     str = str.substring(0,i) + last + str.substring(i + 1, j) + first + str.substring(j + 1, str.length);
-    // return str;
     return recRevStr(str, i + 1, j - 1);
 };
 
-// console.log(recRev("hello".split('')));
 console.log(recRevStr("hello"));
 console.log(recRevStr("abcdefghijklm"));
