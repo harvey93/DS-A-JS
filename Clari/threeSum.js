c5b0adc511f0567a858d9ee0b14d1fd36fdf1217
Hrant <harvey.mirijanyan@gmail.com>
2017-06-19 17:25:53 -0700

    Finish threeSum

--- Clari/threeSum.js
+++ Clari/threeSum.js
@@ -1,14 +1,18 @@
 const threeSum = arr => {
     let set = new Set();
     let res = [];
-    for(let i = 1; i < arr.length - 1; i++) {
+    for(let i = 0; i < arr.length - 1; i++) {
         for(let j = i + 1; j < arr.length; j++) {
             let sum = arr[i] + arr[j];
-            if(set.has([0 - sum])) {
-                
+            if(set.has(0 - sum)) {
+                res.push([0 - sum,arr[i], arr[j]]);
+                // console.log('in');
             }
+            set.add(arr[i]);
+            set.add(arr[j]);
         }
     }
+    return res;
 };
 
 console.log(threeSum([4,-3,-1,10, -8, -2, 50, 100, 200, 321]));
