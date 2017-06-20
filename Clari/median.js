6afe7d64b00fa6d87e364d630b98a00928329269
Hrant <harvey.mirijanyan@gmail.com>
2017-06-19 23:17:55 -0700

    Add Fisher Yates Shuffle

--- Clari/median.js
+++ Clari/median.js
@@ -8,4 +8,33 @@ const median = arr => {
     }
 };
 
-console.log(median([3,7,8,4,5,2,6,1]));
+// quickselect
+
+const quickMedian = arr => {
+    let mid = Math.floor(arr.length/ 2);
+    if(arr.length % 2 === 0 ) {
+        return true;
+    } else {
+        return quickSelect(arr, Math.floor(arr.length / 2));
+    }
+};
+
+const quickSelect = (arr, k) => {
+    return k;
+};
+
+const shuffle = arr => {
+    for(let i = arr.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+};
+
+let arr = [1,2,3,4,5,6,7];
+shuffle(arr);
+// console.log(arr);
+
+console.log(median(arr));
+// console.log(quickMedian(arr));
