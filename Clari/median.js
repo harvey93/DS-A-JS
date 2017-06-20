fbb7d65b5586c26aa80b0dca26f8b59d64811197
Hrant <harvey.mirijanyan@gmail.com>
2017-06-20 11:58:08 -0700

    Add quickselect for odd median

--- Clari/median.js
+++ Clari/median.js
@@ -12,15 +12,61 @@ const median = arr => {
 
 const quickMedian = arr => {
     let mid = Math.floor(arr.length/ 2);
+    // console.log(arr.length % 2);
     if(arr.length % 2 === 0 ) {
         return true;
     } else {
-        return quickSelect(arr, Math.floor(arr.length / 2));
+        return quickSelect(arr, mid);
     }
 };
 
 const quickSelect = (arr, k) => {
-    return k;
+    shuffle(arr);
+    let lo = 0;
+    let hi = arr.length - 1;
+    while(hi > lo) {
+        let j = partition(arr, lo, hi);
+        if(j < k) {
+            lo = j + 1;
+        } else if(j > k){
+            hi = j - 1;
+        } else {
+            return arr[k];
+        }
+    }
+    // console.log(arr);
+    // return arr;
+};
+
+const partition = (arr, lo, hi) => {
+    let i = lo;
+    let j = hi + 1;
+    while (true) {
+        while(less(arr[++i], arr[lo])){
+            if(i === hi) { break; }
+        }
+
+        while(less(arr[lo], arr[--j])){
+            if(j === lo) { break ;}
+        }
+
+        if(i >= j) { break; }
+        exch(arr, i, j);
+    }
+    exch(arr, lo, j);
+    // return arr;
+    return j;
+};
+
+const less = (val1, val2) => {
+    return val1 < val2;
+};
+
+const exch = (arr, i, j ) => {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+    return arr;
 };
 
 const shuffle = arr => {
@@ -32,9 +78,9 @@ const shuffle = arr => {
     }
 };
 
-let arr = [1,2,3,4,5,6,7];
-shuffle(arr);
+let arr = [1,2,3,4,5,6,7,123,12,13];
+// shuffle(arr);
 // console.log(arr);
 
-console.log(median(arr));
-// console.log(quickMedian(arr));
+// console.log(median(arr));
+console.log(quickMedian(arr));
