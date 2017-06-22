dfcfa4a672a0ab65d3cfb3b70ef89909db6569a4
Hrant <harvey.mirijanyan@gmail.com>
2017-06-22 16:11:14 -0700

    Add binary

--- Problems/binary.js
+++ Problems/binary.js
@@ -1,5 +1,13 @@
 const binary = num => {
-
+    let res = [];
+    while (num !== 0) {
+        res.unshift(num % 2);
+        num = Math.floor( num / 2);
+    }
+    return res.join('');
+    
 };
+
+console.log(binary(5));
 
 // Design Twitter, median in unsorted array, . Evaluate a poker hand, fermi estimation, lru cache, 3 sum, 
