d635fcd7ad938245e965be8ffacc12bdcb5e9537
Hrant <harvey.mirijanyan@gmail.com>
2017-06-23 15:11:17 -0700

    Add base case for fibsMemo

--- Problems/fibsDP.js
+++ Problems/fibsDP.js
@@ -9,8 +9,17 @@ const fibs = num => {
 
 // console.log(fibs(100));
 
-const fibsMemo = (num, memo) => {
-
+const fibsMemo = (num, memo = {}) => {
+    if(num === 1) {
+        return 0;
+    } else if (num === 2) {
+        return 1;
+    }
+    if(memo[num]) {
+        return memo[num];
+    } 
+    memo[num] = fibsMemo(num - 1, memo) + fibs(num - 2, memo);
+    return memo[num];
 };
 
-console.log(fibsMemo(100));
+console.log(fibsMemo(3));
